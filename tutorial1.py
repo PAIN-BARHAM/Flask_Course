b5f730ef1419987b6e2e4a4a182ee6c0a347b486
PAIN-BARHAM <mohammedbrham98@gmail.com>
2022-11-20 21:16:30 +0300

    Adding tutorial 2

--- tutorial1.py
+++ tutorial1.py
@@ -1,21 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask("__name__")
 
 
-@app.route("/")
-def home():
-    return "Hello! this is the main page <h1>HELLO<h1>"
-
-
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+def home(name):
+    return render_template("index.html", content=["time", "John", "San"])
 
 
 if __name__ == "__main__":
